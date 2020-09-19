52d489b77bb24a7649b1869c6770348106ae0093
iamhujiebin <458249864@qq.com>
2020-09-19 23:09:02 +0800

    my-miniapp

---
+++ pages/component/index.js
@@ -0,0 +1,73 @@
+//index.js
+//获取应用实例
+const app = getApp()
+
+Page({
+  data: {
+    background: [
+      '/image/b1.jpg',
+      '/image/b2.jpg',
+      '/image/b3.jpg'
+    ],
+    selectTops: [
+      '/image/s1.png',
+      '/image/s2.png'
+    ],
+    selectBottom: "/image/s3.png",
+    newestList: [{
+        id: 1,
+        name: "瓜子 100g",
+        cost: "￥ 1",
+        img: "/image/s5.png"
+      },
+      {
+        id: 2,
+        name: "芹菜 半斤",
+        cost: "￥ 0.02",
+        img: "/image/s6.png"
+      },
+      {
+        id: 3,
+        name: "素米 375g",
+        cost: "￥ 0.03",
+        img: "/image/s4.png"
+      },
+      {
+        id: 4,
+        name: "瓜子 100g",
+        cost: "￥ 0.01",
+        img: "/image/s4.png"
+      },
+      {
+        id: 5,
+        name: "芹菜 半斤",
+        cost: "￥ 0.02",
+        img: "/image/s5.png"
+      },
+      {
+        id: 6,
+        name: "瓜子 100g",
+        cost: "￥ 1",
+        img: "/image/s6.png"
+      }
+    ]
+  },
+  onLoad: function () {},
+  queryCategoryClick: function (e) {
+    var index = e.currentTarget.dataset.index
+    console.log(index)
+    // wx.navigateTo({
+    // url: '../queryResult/queryResult?queryBean=' + queryBean,
+    // })
+  },
+  queryItemClick: function (e) {
+    var index = e.currentTarget.dataset.index
+    var name = this.data.newestList[index].name
+    var cost = this.data.newestList[index].cost
+    var id = this.data.newestList[index].id
+    var img = this.data.newestList[index].img
+    wx.navigateTo({
+      url: '/pages/component/detail/detail?id=' + id + '&name=' + name + '&img=' + img + '&cost=' + cost
+    })
+  }
+})
