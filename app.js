3bce03e7c05330cfb6546dabb6ee6ec859fa4ef3
iamhujiebin <458249864@qq.com>
2020-09-23 10:51:12 +0800

    feat（商城）：我的页面

--- app.js
+++ app.js
@@ -1,15 +1,30 @@
 //app.js
 App({
   onLaunch: function () {
-    console.log('App Launch')
-  },
-  onShow: function () {
-    console.log('App Show')
-  },
-  onHide: function () {
-    console.log('App Hide')
+    wx.login({
+        success: res => {
+          console.log("login success", res)
+        }
+      }),
+      wx.getSetting({
+        success: res => {
+          if (res.authSetting['scope.userInfo']) {
+            wx.getUserInfo({
+              success: res => {
+                this.globalData.userInfo = res.userInfo
+
+                // 由于 getUserInfo 是网络请求，可能会在 Page.onLoad 之后才返回
+                // 所以此处加入 callback 以防止这种情况
+                if (this.userInfoReadyCallback) {
+                  this.userInfoReadyCallback(res)
+                }
+              }
+            })
+          }
+        }
+      })
   },
   globalData: {
-    hasLogin: false
+    userInfo: null
   }
 })
